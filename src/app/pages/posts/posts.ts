f5d43347a251f4fdae2fdeb202b68f300495d371
amaal.yasser <amal.yasser.abdelkader@gmail.com>
2025-09-21 00:16:44 +0300

    Edit operation

--- src/app/pages/posts/posts.ts
+++ src/app/pages/posts/posts.ts
@@ -18,8 +18,9 @@ export class Posts {
     title: '',
     body: '',
   };
+  editIndex!: number;
 
-  handleAddPost() {
+  /*handleAddPost() {
     debugger;
       this.postsList.push({
         userId: 1, 
@@ -30,6 +31,29 @@ export class Posts {
   localStorage.setItem('postsList', JSON.stringify(this.postsList));
       this.newPost = { id: null, title: '', body: '' };
     }
+*/
+handleAddPost() {
+  debugger
+  if ( this.editIndex !== -1 && this.editIndex !== undefined) {
+    this.postsList[this.editIndex] = {
+      id: this.newPost.id,
+      title: this.newPost.title,
+      body: this.newPost.body,
+    };
+  } else {
+    // Add
+    this.postsList.push({
+      id: this.newPost.id,
+      title: this.newPost.title,
+      body: this.newPost.body,
+    });
+  }
+
+  localStorage.setItem('postsList', JSON.stringify(this.postsList));
+
+  this.newPost = { id: null, title: '', body: '' };
+  this.editIndex= -1;
+}
 
 
   deletePost(index: number) {
@@ -37,10 +61,15 @@ export class Posts {
       localStorage.setItem('postsList', JSON.stringify(this.postsList));
   }
 
-  editPost(index: number) {
-  
-  }
+ editPost(index: number) {
+  this.newPost = { 
+    id: this.postsList[index].id, 
+    title: this.postsList[index].title, 
+    body: this.postsList[index].body 
+  };
 
+  this.editIndex = index;
+}
   ngOnInit() {
   const savedPosts = localStorage.getItem('postsList');
   if (savedPosts) {
